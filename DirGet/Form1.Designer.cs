20386b495ebf6ab7826b2171094e880d8c3711d8
Ben Blain <mail@servc.eu>
2016-05-15 15:06:27 +0100

    Add major project files

---
+++ DirGet/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DirGet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getBtn = new System.Windows.Forms.Button();
+            this.uriBox = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.getNoRootBtn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.logErrorsChk = new System.Windows.Forms.CheckBox();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.adminBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // getBtn
+            // 
+            this.getBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.getBtn.Location = new System.Drawing.Point(444, 14);
+            this.getBtn.Name = "getBtn";
+            this.getBtn.Size = new System.Drawing.Size(39, 23);
+            this.getBtn.TabIndex = 0;
+            this.getBtn.Text = "Get";
+            this.getBtn.UseVisualStyleBackColor = true;
+            this.getBtn.Click += new System.EventHandler(this.getBtn_Click);
+            // 
+            // uriBox
+            // 
+            this.uriBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uriBox.Location = new System.Drawing.Point(13, 16);
+            this.uriBox.Name = "uriBox";
+            this.uriBox.Size = new System.Drawing.Size(389, 20);
+            this.uriBox.TabIndex = 1;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseBtn.Location = new System.Drawing.Point(408, 14);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(30, 23);
+            this.browseBtn.TabIndex = 2;
+            this.browseBtn.Text = "...";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(389, 225);
+            this.listBox1.TabIndex = 3;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearBtn.Location = new System.Drawing.Point(408, 72);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveBtn.Location = new System.Drawing.Point(408, 101);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 5;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // getNoRootBtn
+            // 
+            this.getNoRootBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.getNoRootBtn.Location = new System.Drawing.Point(408, 43);
+            this.getNoRootBtn.Name = "getNoRootBtn";
+            this.getNoRootBtn.Size = new System.Drawing.Size(75, 23);
+            this.getNoRootBtn.TabIndex = 6;
+            this.getNoRootBtn.Text = "Omit Root";
+            this.getNoRootBtn.UseVisualStyleBackColor = true;
+            this.getNoRootBtn.Click += new System.EventHandler(this.getNoRootBtn_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Title = "Save List As...";
+            // 
+            // logErrorsChk
+            // 
+            this.logErrorsChk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.logErrorsChk.AutoSize = true;
+            this.logErrorsChk.Location = new System.Drawing.Point(408, 130);
+            this.logErrorsChk.Name = "logErrorsChk";
+            this.logErrorsChk.Size = new System.Drawing.Size(90, 30);
+            this.logErrorsChk.TabIndex = 7;
+            this.logErrorsChk.Text = "Log Access \r\nDenied Errors";
+            this.logErrorsChk.UseVisualStyleBackColor = true;
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.copyBtn.Location = new System.Drawing.Point(408, 166);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(75, 44);
+            this.copyBtn.TabIndex = 8;
+            this.copyBtn.Text = "Copy Selected";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
+            // 
+            // adminBtn
+            // 
+            this.adminBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.adminBtn.Location = new System.Drawing.Point(408, 216);
+            this.adminBtn.Name = "adminBtn";
+            this.adminBtn.Size = new System.Drawing.Size(75, 44);
+            this.adminBtn.TabIndex = 9;
+            this.adminBtn.Text = "Restart as Administrator";
+            this.adminBtn.UseVisualStyleBackColor = true;
+            this.adminBtn.Click += new System.EventHandler(this.adminBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 281);
+            this.Controls.Add(this.adminBtn);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.logErrorsChk);
+            this.Controls.Add(this.getNoRootBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.uriBox);
+            this.Controls.Add(this.getBtn);
+            this.MaximumSize = new System.Drawing.Size(1000, 500);
+            this.MinimumSize = new System.Drawing.Size(510, 320);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Directory Lister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getBtn;
+        private System.Windows.Forms.TextBox uriBox;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button getNoRootBtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox logErrorsChk;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.Button adminBtn;
+    }
+}
+
---
+++ DirGet/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace DirGet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            adminBtn.Visible = !(new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void getBtn_Click(object sender, EventArgs e)
+        {
+            ListDirectories(false);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Text = @"Directory Lister";
+        }
+
+        private void getNoRootBtn_Click(object sender, EventArgs e)
+        {
+            ListDirectories(true);
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            try
+            {
+                var sw = new StreamWriter(saveFileDialog1.FileName);
+                for (var i = 0; i < listBox1.Items.Count - 1; i++)
+                {
+                    sw.WriteLine(listBox1.Items[i]);
+                }
+                sw.Close();
+                sw.Dispose();
+            }
+            catch (Exception ex)
+            {
+                if (Debugger.IsAttached)
+                    throw;
+                MessageBox.Show(@"Cannot write to file:" + Environment.NewLine + ex.Message);
+            }
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = uriBox.Text;
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                uriBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// Lists the requested directories and adds them to the ListBox.
+        /// </summary>
+        /// <param name="shouldOmitRoot">True if the root directory should not be shown in the result.</param>
+        private void ListDirectories(bool shouldOmitRoot)
+        {
+            if (!Directory.Exists(uriBox.Text))
+            {
+                MessageBox.Show(@"Directory does not exist or access is denied.", @"Directory Lister",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (listBox1.Items.Count > 0 && MessageBox.Show(@"Keep current items?", @"Directory Lister", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                listBox1.Items.Clear();
+
+            Enabled = false;
+            Text = @"Directory Lister (Working. Please wait...)";
+            
+            var dirs = DirectoryList(uriBox.Text, true, null);
+
+            try
+            {
+                foreach (var dir in dirs.Where(dir => dir != null))
+                {
+                    listBox1.Items.Add(shouldOmitRoot ? dir.Replace(uriBox.Text, "") : dir);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.InnerException + Environment.NewLine +
+                                ex.Source);
+                if (Debugger.IsAttached)
+                    throw;
+            }
+
+            Enabled = true;
+            Text = $"Directory Lister ({listBox1.Items.Count} items)";
+        }
+
+        /// <summary>
+        /// Returns a list of directories under RootDirectory 
+        /// </summary>
+        /// <param name="rootDirectory">The starting directory.</param>
+        /// <param name="searchAllDirectories">When true, all sub directories will be searched as well.</param>
+        /// <param name="filter">filter to be done on directory. use null for no filtering.</param>
+        /// <returns>A list of directories under RootDirectory</returns>
+        private List<string> DirectoryList(string rootDirectory, bool searchAllDirectories, Predicate<string> filter)
+        {
+            var retList = new List<string>();
+
+            DirectoryInfo[] di;
+            try
+            {
+                di = new DirectoryInfo(rootDirectory).GetDirectories();
+            }
+            catch (Exception ex)
+            {
+                if (Debugger.IsAttached)
+                    throw;
+
+                if (logErrorsChk.Checked) retList.Add("> " + ex.Message);
+
+                return retList;
+            }
+            
+            foreach (var folder in di)
+            {
+                try
+                {
+                    if ((filter == null) || (filter(folder.FullName)))
+                    {
+                        retList.Add(folder.FullName);
+
+                        if (searchAllDirectories)
+                        {
+                            var l = DirectoryList(folder.FullName, true, filter);
+                            if (l.Count > 0)
+                                retList.AddRange(l);
+                        }
+                    }
+                }
+
+                catch (UnauthorizedAccessException ex)
+                {
+                    if (logErrorsChk.Checked) retList.Add("> " + ex.Message);
+                }
+
+                catch (Exception)
+                {
+                    if (Debugger.IsAttached)
+                        throw;
+                }
+            }
+            
+            return retList;
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+                Clipboard.SetText(listBox1.SelectedItem.ToString());
+        }
+
+        private void adminBtn_Click(object sender, EventArgs e)
+        {
+            var info = new ProcessStartInfo(Assembly.GetEntryAssembly().Location)
+            {
+                Verb = "runas",
+            };
+
+            var process = new Process
+            {
+                StartInfo = info
+            };
+
+            try
+            {
+                process.Start();
+                Close();
+            }
+            catch (Exception)
+            {
+                // User pressed 'no' at UAC prompt
+            }
+        }
+    }
+}
